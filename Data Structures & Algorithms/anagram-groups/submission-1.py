09ba91109e37e1ff4807310712427c8b86f8c2d0
Tobi <40189569+tk-tobi@users.noreply.github.com>
2026-04-11 20:50:09 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagram_dict = {} # {sorted_str : [index]}
+
+        for word in strs:
+            new_word = "".join(sorted(word))
+            if new_word in anagram_dict:
+                anagram_dict[new_word].append(word)
+            else:
+                anagram_dict[new_word] = [word]
+        
+        return list(anagram_dict.values())
+        
+        
