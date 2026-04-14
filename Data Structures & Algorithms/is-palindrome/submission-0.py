d317a3f1785f9a17fc4ee899c3f645d3eea2f6d1
Tobi <40189569+tk-tobi@users.noreply.github.com>
2026-04-13 23:45:08 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = s.replace(" ", "").lower().strip('.?')
+        reverse_s = "".join([string[i] for i in range(len(string) - 1, -1, -1)])
+        
+        for i in range(len(string)):
+            if string[i] != reverse_s[i]:
+                return False
+        return True 
+        
