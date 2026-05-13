e917e0608215b1a0e62c9937e69ee8122cdb74ea
Tobi <40189569+tk-tobi@users.noreply.github.com>
2026-05-13 00:23:31 -0400

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        triplet_store = []
+        nums = list(sorted(nums))
+
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                complement = (nums[i] + nums[j]) * - 1
+                if complement in nums[j + 1:] and [nums[i], nums[j], complement] not in triplet_store:
+                    triplet_store.append([nums[i], nums[j], complement])
+                break
+        
+        return triplet_store
+        
